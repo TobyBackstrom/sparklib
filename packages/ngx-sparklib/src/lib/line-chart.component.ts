7649d5aff8284c0a981470efa890609f105bac8b
Toby Backstrom <torbjorn.backstrom@teknikfabriken.se>
2023-05-19 19:15:19 +0200

    added ngx-sparklib

---
+++ packages/ngx-sparklib/src/lib/line-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sparklib-line-chart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.scss'],
+})
+export class LineChartComponent {}
