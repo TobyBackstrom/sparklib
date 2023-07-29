d973587dda9617ebbe8a6db27cca25e20a603a05
Toby Backstrom <torbjorn.backstrom@teknikfabriken.se>
2023-07-29 15:54:07 +0200

    chore: misc config for react component testing

--- packages/react-sparklib/jest.config.ts
+++ packages/react-sparklib/jest.config.ts
@@ -1,13 +1,33 @@
-import '@testing-library/jest-dom/extend-expect';
-
 /* eslint-disable */
 export default {
   displayName: 'react-sparklib',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      {
+        presets: [
+          '@nx/react/babel',
+          ['@babel/preset-env', { loose: true }],
+          '@babel/preset-typescript',
+        ],
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/react-sparklib',
+  moduleNameMapper: {
+    '^d3-array$': 'node_modules/d3-array/dist/d3-array.min.js',
+    '^d3-color$': 'node_modules/d3-color/dist/d3-color.min.js',
+    '^d3-format$': 'node_modules/d3-format/dist/d3-format.min.js',
+    '^d3-interpolate$':
+      'node_modules/d3-interpolate/dist/d3-interpolate.min.js',
+    '^d3-path$': 'node_modules/d3-path/dist/d3-path.min.js',
+    '^d3-scale$': 'node_modules/d3-scale/dist/d3-scale.min.js',
+    '^d3-shape$': 'node_modules/d3-shape/dist/d3-shape.min.js',
+    '^d3-time$': 'node_modules/d3-time/dist/d3-time.min.js',
+    '^d3-time-format$':
+      'node_modules/d3-time-format/dist/d3-time-format.min.js',
+  },
 };
--- packages/react-sparklib/src/lib/line-chart.spec.tsx
+++ packages/react-sparklib/src/lib/line-chart.spec.tsx
@@ -1,19 +1,24 @@
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import LineChart from './line-chart';
-import '@testing-library/jest-dom/extend-expect';
 
-test('renders without crashing', () => {
-  const { container } = render(
-    <LineChart
-      values={[
-        [1, 2],
-        [2, 3],
-        [3, 4],
-        [4, 5],
-        [5, 6],
-      ]}
-    />,
-  );
+afterEach(() => {
+  cleanup();
+});
 
-  expect(container).toBeInTheDocument();
+describe('LineChart', () => {
+  test('should render successfully', () => {
+    const { baseElement } = render(
+      <LineChart
+        values={[
+          [1, 2],
+          [2, 3],
+          [3, 4],
+          [4, 5],
+          [5, 6],
+        ]}
+      />,
+    );
+    expect(baseElement).toBeInTheDocument();
+  });
 });
