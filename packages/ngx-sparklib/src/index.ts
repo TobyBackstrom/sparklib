57bcb21aed86f80398522e7c18e2e07ce3668155
Toby Backstrom <torbjorn.backstrom@teknikfabriken.se>
2023-08-12 16:53:31 +0200

    feat: re-export deps

--- packages/ngx-sparklib/src/index.ts
+++ packages/ngx-sparklib/src/index.ts
@@ -1,2 +1,16 @@
 export * from './lib/line-chart.component';
 export * from './lib/stripe-chart.component';
+export {
+  datumLine,
+  lineChart,
+  lineProperties,
+  linearGradient,
+  margins,
+  DatumLine,
+  DatumLineBuilder,
+  LinearGradient,
+  LinearGradientBuilder,
+  Margins,
+  MarginsBuilder,
+  Range,
+} from 'sparklib';
--- packages/react-sparklib/src/index.ts
+++ packages/react-sparklib/src/index.ts
@@ -1,2 +1,16 @@
 export * from './lib/line-chart';
 export * from './lib/stripe-chart';
+export {
+  datumLine,
+  lineChart,
+  lineProperties,
+  linearGradient,
+  margins,
+  DatumLine,
+  DatumLineBuilder,
+  LinearGradient,
+  LinearGradientBuilder,
+  Margins,
+  MarginsBuilder,
+  Range,
+} from 'sparklib';
