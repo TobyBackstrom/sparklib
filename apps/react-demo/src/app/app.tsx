a676ff37e5f7acb7e51426e771b504cd0235c390
Toby Backstrom <torbjorn.backstrom@teknikfabriken.se>
2023-08-26 12:06:24 +0200

    feat: add react-demo

---
+++ apps/react-demo/src/app/app.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return <div></div>;
+}
+
+export default App;
