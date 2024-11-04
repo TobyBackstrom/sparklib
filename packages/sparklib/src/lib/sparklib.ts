77b3b66e3ea6ff9df81bcd497becb5ef70ab39f8
Toby Backstrom <torbjorn.backstrom@teknikfabriken.se>
2024-11-04 12:25:25 +0100

    feat: include access to more factory functions via sparklib

--- packages/sparklib/src/lib/sparklib.ts
+++ packages/sparklib/src/lib/sparklib.ts
@@ -1,13 +1,28 @@
+import { axis } from './axis';
 import { barChart } from './bar-chart';
+import { datumLine } from './builders/datum-line-builder';
+import { lineProperties } from './builders/line-properties-builder';
+import { linearGradient } from './builders/linear-gradient-builder';
+import { margins } from './builders/margins-builder';
 import { lineChart } from './line-chart';
 import { stripeChart } from './stripe-chart';
-import { axis } from './axis';
+import { getIndicesForPixelX } from './utils/get-indices-for-pixel-x';
 
 export function sparklib() {
   return {
+    // Charts
     axis,
     barChart,
     lineChart,
     stripeChart,
+
+    /// Builders
+    datumLine,
+    linearGradient,
+    lineProperties,
+    margins,
+
+    // Utils
+    getIndicesForPixelX,
   };
 }
