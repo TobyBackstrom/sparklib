a161a7b0863d9864819d2894ec28f8d19012f5e3
Toby Backstrom <torbjorn.backstrom@teknikfabriken.se>
2024-11-24 12:11:01 +0100

    feat(web-demo): use defer for scripts

--- apps/web-demo/src/index.ts
+++ apps/web-demo/src/index.ts
@@ -5,54 +5,52 @@ const data = [
   8, 6, 5, 4, 2, 1, 1, 1, 6, 7, 7, 8, 9, 0,
 ];
 
-document.addEventListener('DOMContentLoaded', () => {
-  const div0 = document.createElement('div');
-  document.body.appendChild(div0);
+const div0 = document.createElement('div');
+document.body.appendChild(div0);
 
-  const chart0 = sparklib()
-    .lineChart()
-    .width(250)
-    .height(40)
-    .background('lightyellow')
-    .fillStyle(
-      sparklib()
-        .linearGradient(0, 0, 250, 0)
-        .addColorStop(0, 'lightgreen')
-        .addColorStop(1, 'black'),
-    )
-    .render(data);
-  div0.appendChild(chart0);
+const chart0 = sparklib()
+  .lineChart()
+  .width(250)
+  .height(40)
+  .background('lightyellow')
+  .fillStyle(
+    sparklib()
+      .linearGradient(0, 0, 250, 0)
+      .addColorStop(0, 'lightgreen')
+      .addColorStop(1, 'black'),
+  )
+  .render(data);
+div0.appendChild(chart0);
 
-  const br1 = document.createElement('br');
-  document.body.appendChild(br1);
-  const div1 = document.createElement('div');
-  document.body.appendChild(div1);
+const br1 = document.createElement('br');
+document.body.appendChild(br1);
+const div1 = document.createElement('div');
+document.body.appendChild(div1);
 
-  const chart1 = sparklib()
-    .barChart()
-    .width(250)
-    .height(40)
-    .background('lightyellow')
-    .fillStyle(
-      sparklib()
-        .linearGradient(0, 0, 250, 0)
-        .addColorStop(0, 'lightgreen')
-        .addColorStop(1, 'black'),
-    )
-    .render(data);
-  div1.appendChild(chart1);
+const chart1 = sparklib()
+  .barChart()
+  .width(250)
+  .height(40)
+  .background('lightyellow')
+  .fillStyle(
+    sparklib()
+      .linearGradient(0, 0, 250, 0)
+      .addColorStop(0, 'lightgreen')
+      .addColorStop(1, 'black'),
+  )
+  .render(data);
+div1.appendChild(chart1);
 
-  const br2 = document.createElement('br');
-  document.body.appendChild(br2);
-  const div2 = document.createElement('div');
-  document.body.appendChild(div2);
+const br2 = document.createElement('br');
+document.body.appendChild(br2);
+const div2 = document.createElement('div');
+document.body.appendChild(div2);
 
-  const chart2 = sparklib()
-    .stripeChart()
-    .width(250)
-    .height(40)
-    .gradientColors(['white', 'lightgreen', 'black'])
-    .nGradientColorLevels(data.length)
-    .render(data);
-  div2.appendChild(chart2);
-});
+const chart2 = sparklib()
+  .stripeChart()
+  .width(250)
+  .height(40)
+  .gradientColors(['white', 'lightgreen', 'black'])
+  .nGradientColorLevels(data.length)
+  .render(data);
+div2.appendChild(chart2);
